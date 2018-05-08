8622194d4af97bd141fb80e6b7f5079bbbf8c63b
Jeremy Greer <jeremy.greer.atl@gmail.com>
2018-05-07 22:03:35 -0400

    Add hover state to filled cells

--- index.js
+++ index.js
@@ -7,13 +7,42 @@
   ]
 
   const drawHive = (hive) => {
-    const cellFactory = (filled) => {
+    const getIntAttr = (element, attr) => {
+      return parseInt(element.getAttribute(attr))
+    }
+
+    const handleCellMouseOver = (event) => {
+      const target = event.target
+      const columnIndex = getIntAttr(target, 'data-columnIndex')
+      const rowIndex = getIntAttr(target, 'data-rowIndex')
+      console.log(columnIndex, rowIndex)
+
+      target.className += ' hover'
+    }
+
+    const handleCellMouseOut = (event) => {
+      const target = event.target
+      const columnIndex = getIntAttr(target, 'data-columnIndex')
+      const rowIndex = getIntAttr(target, 'data-rowIndex')
+      console.log(columnIndex, rowIndex)
+
+      const className = target.className.split(' ').filter(x => x !== 'hover').join(' ')
+      target.className = className
+    }
+
+    const cellFactory = (filled, columnIndex, rowIndex) => {
       const div = document.createElement('div')
       const className = 'cell' + (filled
         ? ' filled'
         : ''
       )
       div.className = className
+      if (filled) {
+        div.addEventListener('mouseover', handleCellMouseOver)
+        div.addEventListener('mouseout', handleCellMouseOut)
+      }
+      div.setAttribute('data-columnIndex', columnIndex)
+      div.setAttribute('data-rowIndex', rowIndex)
       return div
     }
 
@@ -24,7 +53,7 @@
 
     hive.forEach((row, rowIndex) => {
       row.forEach((value, columnIndex) => {
-        const cell = cellFactory(!!value)
+        const cell = cellFactory(!!value, columnIndex, rowIndex)
         const x = (columnIndex * 3 / 4 * size) + padding * columnIndex
         const y = (rowIndex * size + (columnIndex % 2 === 1 ? (1 / 2 * size) : 0))
           + padding * rowIndex
@@ -34,7 +63,6 @@
       })
     })
   }
-
 
   drawHive(hive)
 })()
