8bcb6bc27a0af2599822d26983c0cfebd63fe965
Jeremy Greer <jeremy.greer.atl@gmail.com>
2018-05-07 22:39:59 -0400

    Show adjacent cells

--- index.js
+++ index.js
@@ -7,27 +7,87 @@
   ]
 
   const drawHive = (hive) => {
+    // This will mirror the hive.
+    const elements = []
+
     const getIntAttr = (element, attr) => {
       return parseInt(element.getAttribute(attr))
     }
 
+    const getCoordsFromElement = (element) => {
+      const columnIndex = getIntAttr(element, 'data-columnIndex')
+      const rowIndex = getIntAttr(element, 'data-rowIndex')
+      return {
+        x: columnIndex,
+        y: rowIndex,
+      }
+    }
+
+    const getElementFromCoords = (coords) => {
+      const row = elements[coords.y]
+      if (row) {
+        const element = row[coords.x]
+        return element
+      }
+    }
+
+    const getAdjacentCells = (coords) => {
+      const x = coords.x
+      const y = coords.y
+      const n = y - 1
+      const s = y + 1
+      const wx = x - 1
+      const ex = x + 1
+      const ny = x % 2 === 1 ? y : n
+      const sy = x % 2 === 1 ? s : y
+      return [
+        // N
+        getElementFromCoords({ x: x, y: n }),
+        // S
+        getElementFromCoords({ x: x, y: s }),
+        // NW
+        getElementFromCoords({ x: wx, y: ny }),
+        // NE
+        getElementFromCoords({ x: ex, y: ny }),
+        // SW
+        getElementFromCoords({ x: wx, y: sy }),
+        // SE
+        getElementFromCoords({ x: ex, y: sy }),
+      ]
+        .filter(x => !!x)
+    }
+
+    const addClass = (element, className) => {
+      if (element.className.indexOf(className) === -1) {
+        element.className += (' ' + className)
+      }
+    }
+
     const handleCellMouseOver = (event) => {
       const target = event.target
-      const columnIndex = getIntAttr(target, 'data-columnIndex')
-      const rowIndex = getIntAttr(target, 'data-rowIndex')
-      console.log(columnIndex, rowIndex)
+      const coords = getCoordsFromElement(target)
+      addClass(target, 'hover')
 
-      target.className += ' hover'
+      const adjacentCells = getAdjacentCells(coords)
+      adjacentCells.forEach(cell => {
+        addClass(cell, 'adjacent')
+      })
     }
+
+    const removeClass = (element, className) => {
+      element.className = element.className.split(' ').filter(x => x !== className).join(' ')
+    }
+
 
     const handleCellMouseOut = (event) => {
       const target = event.target
-      const columnIndex = getIntAttr(target, 'data-columnIndex')
-      const rowIndex = getIntAttr(target, 'data-rowIndex')
-      console.log(columnIndex, rowIndex)
+      const coords = getCoordsFromElement(target)
+      removeClass(target, 'hover')
 
-      const className = target.className.split(' ').filter(x => x !== 'hover').join(' ')
-      target.className = className
+      const adjacentCells = getAdjacentCells(coords)
+      adjacentCells.forEach(cell => {
+        removeClass(cell, 'adjacent')
+      })
     }
 
     const cellFactory = (filled, columnIndex, rowIndex) => {
@@ -52,6 +112,7 @@
     const padding = 2
 
     hive.forEach((row, rowIndex) => {
+      const elementRow = []
       row.forEach((value, columnIndex) => {
         const cell = cellFactory(!!value, columnIndex, rowIndex)
         const x = (columnIndex * 3 / 4 * size) + padding * columnIndex
@@ -59,8 +120,10 @@
           + padding * rowIndex
         cell.style.left = x + 'px'
         cell.style.top = y + 'px'
+        elementRow.push(cell)
         root.append(cell)
       })
+      elements.push(elementRow)
     })
   }
 
