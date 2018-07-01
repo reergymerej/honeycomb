dce1f0de64ad6d07f65654fac93dfdd528e60199
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-07-01 13:00:19 -0400

    Pass values into function call

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-(() => {
-  let rows = 12
+((rowNum, colNum, rowCondition, paddingNum, sizeNum) => {
+  let rows = rowNum
 
   const hive = (() => {
     const grid = []
     while (rows--) {
       const row = []
-      let cols = 36
+      let cols = colNum
       while (cols--) {
         row.push(
-          Math.random() > 0.2
+          Math.random() > rowCondition
         )
       }
       grid.push(row)
@@ -118,8 +118,8 @@
 
     const root = document.querySelector('#hive')
 
-    const size = 50
-    const padding = 2
+    const padding = paddingNum
+    const size = sizeNum
 
     hive.forEach((row, rowIndex) => {
       const elementRow = []
@@ -138,4 +138,4 @@
   }
 
   drawHive(hive)
-})()
+})(12, 36, 0.2, 0, 50)
